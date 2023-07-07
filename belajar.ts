7897ef85b2b1408e1e1e0fdcddf8f6fc26bccb78
rizqiagza <rizqiagza@gmail.com>
2023-07-07 18:42:09 +0700

    untuk file belajar upload

---
+++ belajar.ts
@@ -0,0 +1,34 @@
+// //case 1 : menentukan tarif parkiran berdasarkan jenis kendaraan
+// //input
+const tarifMotor : number = 3000
+const tarifMobil : number = 5000
+const tarifBis : number = 10000
+const tarifTruck : number = 15000
+
+//IF
+// if (tarifMotor >= 3000){
+//     console.log("kemahalan cuy")
+// }
+// if (tarifMotor == 3000){
+//     console.log("harga normal")
+// }
+// if(tarifMotor <= 3000){
+//     console.log("kemurahan")
+// }
+// lgsung ke terminal untuk outputnya
+//----> contoh belajar sendiri
+
+const hargaPisang : number = 5000
+const hargaNaga : number = 2000
+const hargaApel : number = 3000
+
+if(hargaApel <= 3000){
+    console.log("kemurahan ini")
+}
+if(hargaApel == 3000){
+    console.log("harga pas")
+}
+if(hargaApel >= 3000){
+    console.log("buset da mahal bgt")
+}
+
